87c7d53c9bbcb98b8030cff9be50aab336257d63
Mantice <nicholas_call@outlook.com>
2023-12-29 09:18:22 -0500

    feat: log and main config

--- src/main/java/dev/paracausal/thetatags/ThetaTags.java
+++ src/main/java/dev/paracausal/thetatags/ThetaTags.java
@@ -2,16 +2,22 @@ package dev.paracausal.thetatags;
 
 import dev.paracausal.thetatags.configuration.Config;
 import dev.paracausal.thetatags.tags.TagBlacklist;
+import dev.paracausal.thetatags.utils.logger.ThetaLog;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ThetaTags extends JavaPlugin {
 
+    private Config configYml;
     private Config bracketsYml;
     private Config blacklistYml;
+
+    private ThetaLog log;
     private TagBlacklist tagBlacklist;
 
     @Override
     public void onLoad() {
+        this.configYml = new Config(this, "config");
+        this.log = new ThetaLog(this);
         this.bracketsYml = new Config(this, "brackets");
         this.blacklistYml = new Config(this, "blacklist");
         this.tagBlacklist = new TagBlacklist(this);
@@ -27,9 +33,11 @@ public class ThetaTags extends JavaPlugin {
 
     }
 
+    public Config getConfigYml() { return configYml; }
     public Config getBracketsYml() { return bracketsYml; }
     public Config getBlacklistYml() { return blacklistYml; }
 
+    public ThetaLog getLog() { return log; }
     public TagBlacklist getTagBlacklist() { return tagBlacklist; }
 
 }
---
+++ src/main/java/dev/paracausal/thetatags/utils/logger/ThetaLog.java
@@ -0,0 +1,68 @@
+package dev.paracausal.thetatags.utils.logger;
+
+import dev.paracausal.thetatags.ThetaTags;
+import dev.paracausal.thetatags.configuration.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
+public class ThetaLog {
+
+    public enum Level {
+        INFO, SUCCESS("&a"), ERROR("&c");
+        private final String color;
+        Level(@NotNull final String color) { this.color = color; }
+        Level() { this.color = ""; }
+    }
+
+    private static final String PREFIX = "[ThetaTags] ";
+    private static final String DEBUG = "[&ei&r] ";
+
+    private static boolean format = true;
+    private static boolean debugMode = false;
+
+    private final ThetaTags plugin;
+
+    public ThetaLog(@NotNull final ThetaTags plugin) {
+        this.plugin = plugin;
+        reload();
+    }
+
+    public void reload() {
+        final Config config = plugin.getConfigYml();
+        format = config.options().getBoolean("logging.format", true);
+        debugMode = config.options().getBoolean("logging.debug", false);
+    }
+
+    private static void send(@NotNull final Level level, final boolean debug, @NotNull final String... input) {
+        if (debug && !debugMode) return;
+
+        for (final String string : input) {
+            final StringBuilder builder = new StringBuilder(PREFIX);
+            if (debugMode && debug) builder.append(DEBUG);
+            builder.append(level.color).append(string);
+
+            String message = ChatColor.translateAlternateColorCodes('&', builder.toString());
+            if (!format) message = ChatColor.stripColor(message);
+
+            Bukkit.getConsoleSender().sendMessage(message);
+        }
+    }
+
+    public static void log(@NotNull final String... input) {
+        send(Level.INFO, false, input);
+    }
+
+    public static void log(@NotNull final Level level, @NotNull final String... input) {
+        send(level, false, input);
+    }
+
+    public static void debug(@NotNull final String... input) {
+        send(Level.INFO, true, input);
+    }
+
+    public static void debug(@NotNull final Level level, @NotNull final String... input) {
+        send(level, true, input);
+    }
+
+}
